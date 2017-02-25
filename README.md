23c91b94af0ca518a9e8e0f099511c10cfa7ba59
Mike Morris <michael.m.morris@gmail.com>
2017-02-25 17:35:37 -0600

    added Dockerfile, build script and README

---
+++ README.md
@@ -0,0 +1,3 @@
+A Prometheus server Docker image for ARM architectures.
+
+See the official github repo: https://github.com/prometheus/prometheus for supported configuration settings.
---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+ver=${VERSION:="1.5.2"}
+exp_name=prometheus
+
+NAME=$(basename $(dirname $PWD/Dockerfile))
+URL="https://github.com/prometheus/${exp_name}/releases/download/v${ver}/${exp_name}-${ver}.linux-armv7.tar.gz"
+
+wget -N $URL
+tar xzf ${exp_name}-${ver}.linux-armv7.tar.gz --strip-components 1 --wildcards "*/prom*" "*/console*"
+
+docker build -t ${NAME}:${ver} -t ${NAME}:latest .
+
+rm -rf prometheus promtool prometheus.yml console*
